2834dcc0dca7dbe9af290f2694fc5abd1e06f6f9
Substrata <digital_strata@mail.ru>
2023-06-01 02:03:17 +0300

    added solution for task27 (sum of all digits in a number)

---
+++ task02_27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Input()
+{
+    Console.WriteLine("Введите число:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+void DegreeAtoB(int count)
+{
+    int i = 1;
+    int result = 0;
+    while (0 < count)
+    {
+        int a = count % 10;
+        result = result + a;
+        count /= 10;
+    }
+    Console.WriteLine($"Сумма цифр в числе = {result}");
+}
+int number = Input();
+DegreeAtoB(number);
